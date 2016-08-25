e5c381c3b229600acabfcc70f9c0b10a38f046e7
gangxia11 <295143002@qq.com>
2016-08-25 15:43:57 +0800

    '11545'

---
+++ main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by gxia on 2016/8/25.
+ */
+require.config({
+    baseUri:'./vender',
+    paths:{
+        'jquery':'jquery',
+        'avalon':'avalon',
+        'text':'text'
+    }
+})
