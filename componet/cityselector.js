ca570f7f5578e3fdf43fd085005fe38423eff5b2
gangxia11 <295143002@qq.com>
2016-08-26 18:30:57 +0800

    '2'

--- componet/cityselector.js
+++ componet/cityselector.js
@@ -1,52 +1,90 @@
 /**
  * Created by gxia on 2016/8/25.
  */
-define(['avalon', 'text!../../template/city_tpl.html', 'text!../../template/search_tpl.html', 'text!../../template/stab_tpl.html', 'text!../../template/spanel_tpl.html'], function (avalon, tpl, tpl2, tpl3, tpl4) {
+function heredoc(fn) {
+    return fn.toString().replace(/^[^\/]+\/\*!?\s?/, '').
+    replace(/\*\/[^\/]+$/, '').trim().replace(/>\s*</g, '><')
+}
+
+
     avalon.component('ms-grid', {
-        template: tpl,
+        template: heredoc(function(){
+           /*
+           <div>
+            <slot ></slot>
+           </div>
+            */
+        }),
         soleSlot: 'buttonText',
         defaults: {
             buttonText: 'hello',
             key: {
-                city:'上海'
+                CityName:'上海'
             }
 
         }
     })
     avalon.component('ms-search', {
-        template: tpl2,
+        template:heredoc(function(){
+            /*
+             <div>
+               <input type="text" ms-css="{width:@width}" ms-duplex="@key['CityName']" ms-click="@click" ms-keypress="@press">
+               <div>{{@key}}</div>
+             </div>
+             */
+        }),
         defaults: {
             click:function(){
-                console.log('click')
+
             },
             press:function(){
-                console.log('press')
+
             },
         }
     })
 
     avalon.component('ms-stab', {
-        template: tpl3,
+        template: heredoc(function(){
+            /*
+             <div ms-css="{width:'400px'}" class="x_tab">
+               <div class="x_tab_head">
+                 <ul >
+                    <li ms-for="(el,$index) in @data">
+                      <a href="javascript:;" ms-click="@tabChange(el)">{{el}}</a>
+                    </li>
+                 </ul>
+
+               </div>
+               <div class="x_tab_body" ms-for="(el,$index) in @data" ms-visible="el===@defaultHead">
+
+                 <ul >
+
+                   <li ms-for="$index in @data[el]">
+                     <a href="javascript:;" ms-click="@setItem($index)">{{$index['CityName']}}</a>
+                   </li>
+                 </ul>
+               </div>
+
+
+             </div>
+             */
+        }),
         defaults: {
-            defaultHead:'abcdef',
+            defaultHead:'ABCEDF',
+            data:initData,
             tabChange:function(ele){
-                console.log( this.defaultHead)
-                this.defaultHead=ele
+
+                this.defaultHead=ele;
+
+
             },
             setItem:function(obj){
-                this.key['city']=obj['city']
+                this.key['CityName']=obj['CityName']
+            },
+            onInit:function(){
+
             }
         }
     })
-    avalon.component('ms-spanel', {
-        template: tpl4,
-        defaults: {
-
-        }
-    })
-
-    console.log(avalon)
-    console.log(avalon.components)
 
 
-})
